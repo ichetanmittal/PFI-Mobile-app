aa40ad905c03674c862fa3edd6f991dd5e5ba02e
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2024-07-25 11:50:40 +0900

    Initial Commit

---
+++ pfi_test/pfi_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  pfi_testApp.swift
+//  pfi_test
+//
+//  Created by Chetan Mittal on 2024/07/25.
+//
+
+import SwiftUI
+
+@main
+struct pfi_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
